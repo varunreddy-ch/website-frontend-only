af18a6ba37df6a35aa5af9144d4cdbf922193106
Varun Reddy Chanda <56602147+varunreddy-ch@users.noreply.github.com>
2025-07-26 04:36:44 -0400

    Corrected server

--- src/api.js
+++ src/api.js
@@ -1,36 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+	baseURL: "https://api.resumevar.com/api",
+	// baseURL: 'http://localhost:3000/api',
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 // Add request interceptor to include auth token
 API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 // Add response interceptor for error handling
 API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/signin';
-    }
-    return Promise.reject(error);
-  }
+	(response) => response,
+	(error) => {
+		if (error.response?.status === 401) {
+			localStorage.removeItem("token");
+			window.location.href = "/signin";
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default API;
