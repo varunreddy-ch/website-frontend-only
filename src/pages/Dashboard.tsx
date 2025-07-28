00d28729041084166bbe23b71b04e7c2ff29d4fa
Varun Reddy Chanda <56602147+varunreddy-ch@users.noreply.github.com>
2025-07-28 00:00:10 -0400

    Fix: Download pdf as Name_Company.pdf

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -223,7 +223,10 @@ export default function Dashboard() {
 							<div className="flex flex-col sm:flex-row justify-center gap-4">
 								<a
 									href={pdfBlob}
-									download={`${fullName}.pdf`}
+									download={`${fullName}_${companyName
+										.split(" ")
+										.filter(Boolean)
+										.join("_")}.pdf`}
 									className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded text-center"
 								>
 									Download PDF
