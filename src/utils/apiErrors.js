0000bec9c6706bfda508d44e27a94218a100024b
Varun Reddy Chanda <56602147+varunreddy-ch@users.noreply.github.com>
2025-08-10 20:01:33 -0500

    Added rate limit

--- src/utils/apiErrors.js
+++ src/utils/apiErrors.js
@@ -1,5 +1,6 @@
 // utils/apiError.js
 export function getAPIErrorMessage(error, fallback = "Something went wrong.") {
+	console.log(error);
 	if (error?.response?.data?.message) return error.response.data.message;
 	if (error?.response?.data?.error) return error.response.data.error;
 	if (typeof error?.response?.data === "string") return error.response.data;
@@ -7,6 +8,8 @@ export function getAPIErrorMessage(error, fallback = "Something went wrong.") {
 		return "You do not have permission to perform this action.";
 	if (error?.response?.status === 404)
 		return "Resource not found. Please check and try again.";
+	if (error?.response?.status === 429)
+		return "Rate Limit Reached for the minute or the entire day";
 	if (error?.message) return error.message;
 	return fallback;
 }
